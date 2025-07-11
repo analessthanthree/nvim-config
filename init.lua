f066dd622247e1046fbf94b882a68c71f3fff1d7
ana <ana@thinkPad>
2025-07-11 15:18:50 -0600

    Make nvim use nushell in ':!' style commands

--- init.lua
+++ init.lua
@@ -3,8 +3,8 @@ require("config.lazy")
 vim.cmd('color industry')
 
 -- https://www.kiils.dk/en/blog/2024-06-22-using-nushell-in-neovim/
--- vim.opt.shellcmdflag = "-l --stdin --no-newlin"
--- vim.opt.shelltemp = false
+vim.opt.shellcmdflag = "-l --stdin --no-newlin"
+vim.opt.shelltemp = false
 
 -- require('Comment').setup()
 
