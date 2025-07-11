340b92b5e937000815c78e5b85d5a697300b2744
ana <ana@thinkPad>
2025-07-11 15:12:18 -0600

    Add ToggleTerm key mappings

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -13,4 +13,7 @@ wk.add({
   { "<C-j>", "<C-w>j", desc = "<Windowing> To Down Window" },
   { "<C-k>", "<C-w>k", desc = "<Windowing> To Up Window" },
   { "<C-l>", "<C-w>l", desc = "<Windowing> To Right Window" },
+  { "<M-1>", "<cmd>ToggleTerm direction=horizontal<CR>", desc = "<ToggleTerm> Open Horizontal Terminal", mode = {"n", "v", "i", "t"} },
+  { "<M-2>", "<cmd>ToggleTerm direction=tab<CR>", desc = "<ToggleTerm> Open Tab Terminal", mode = {"n", "v", "i", "t"} },
+  { "<M-3>", "<cmd>ToggleTerm direction=float<CR>", desc = "<ToggleTerm> Open Float Terminal", mode = {"n", "v", "i", "t"} },
 })
